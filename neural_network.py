761c33fe4b0f6678abcd6aa0ca271e98fb4d0495
RTFRekola <rtfrekola@gmail.com>
2024-07-08 11:41:05 +0100

    New neural network

--- neural_network.py
+++ neural_network.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 '''
 Created on Thu 6 Jul 2023 at 19:50 UT
-Last modified on Thu 16 May 2024 at 13:13 UT 
+Last modified on Sat 29 Jun 2024 at 16:33 UT 
 
 @author: Rami T. F. Rekola 
 
@@ -21,6 +21,10 @@ import yaml
 
 from datetime import datetime
 from modelling import save_model, split_data
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 from sklearn.model_selection import GridSearchCV, train_test_split
 # from sklearn.preprocessing import StandardScaler
 from tabular_data import load_airbnb
@@ -171,6 +175,93 @@ def train(model, loader, epochs, optimiser_value, learning_rate_value):
 # end train
 
 
+def train_classifier(model, loader, epochs, optimiser_value, learning_rate_value):
+
+    '''
+    This function does training on the neural network model. 
+
+    Inputs:
+    - epochs = the number of epochs to be processed, input with function call
+    - model = the instantiated neural network model to be used, input with 
+              function call
+    - optimiser_value = optimiser
+    - learning_rate_value = learning rate
+    - loader = the dataloader, input with function call
+
+    Internal variables: 
+    - batch = loop parameter
+    - epoch = loop parameter
+    - features = numerical Airbnb features
+    - labels = price per night
+    - prediction = the neural network output
+
+    Returns: 
+    - inference_latency = average time taken to make a prediction
+    - accuracy = average accuracy score
+    - f1_score = average F1 score
+    - precision = average precision score
+    - recall = average recall score
+    '''
+
+    if (optimiser_value == 'SGD'):
+        optimiser = torch.optim.SGD(model.parameters(), lr=learning_rate_value)
+    elif (optimiser_value == 'Adagrad'):
+        optimiser = torch.optim.Adagrad(model.parameters(), lr=learning_rate_value)
+    elif (optimiser_value == 'Adam'):
+        optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate_value)
+    # end if
+    writer = SummaryWriter()
+    batch_idx = 0
+    inference_latency_items = []
+    cross_entropy_items = []
+    accuracy_items = []
+    f1_score__items = []
+    precision_items = []
+    recall_items = []
+    for epoch in range(epochs):
+        accuracy_score = 0 ; i = 0
+        for batch in loader:
+            i += 1
+            features, labels = batch
+            labels = labels.unsqueeze(1)
+            # Find the time it takes to make the prediction
+            datetime_before = datetime.now()
+            prediction = model(features)
+            datetime_after = datetime.now()
+            inference_latency_items.append((datetime_after - datetime_before).total_seconds())
+            # Find cross entropy
+            y_tensor_initial = torch.tensor(y_test.values)
+            prediction_tensor = prediction.float()
+            y_tensor = y_tensor_initial.reshape((128,))
+            print("size, y, pred = ", y_tensor.shape, prediction_tensor.shape)
+            #prediction_tensor = torch.tensor(prediction.values)
+            cross_entropy_items.append(f.cross_entropy(prediction_tensor, y_tensor))
+            # Find accuracy
+            accuracy_items.append(accuracy_score(y_test, prediction))
+            # Find the F1 score
+            f1_score_items.append(f1_score(y_test, prediction))
+            # Find precision
+            precision_items.append(precision_score(y_test, prediction))
+            # Find recall
+            recall_items.append(recall_score(y_test, prediction))
+            # Optimisation step
+            optimiser.step()
+            optimiser.zero_grad()
+            #writer.add_scalar('loss', rmse_loss.item(), batch_idx)
+            batch_idx += 1
+        # end for
+    # end for
+    # Finalise the values for metrics 
+    cross_entropy_value = sum(cross_entropy_items) / len(cross_entropy_items)
+    accuracy = sum(accuracy_items) / len(accuracy_items)
+    f1_score = sum(f1_score_items) / len(f1_score_items)
+    precision = sum(precision_items) / len(precision_items)
+    recall = sum(recall_items) / len(recall_items)
+    inference_latency = sum(inference_latency_items) / len(inference_latency_items)
+    return cross_entropy_value, accuracy, f1_score, precision, recall, inference_latency
+# end train_classifier
+
+
 def get_nn_config():
     '''
     This function reads in a yaml file with hyperparameter values and returns it as a dictionary.
@@ -182,7 +273,7 @@ def get_nn_config():
 # end get_nn_config
 
 
-def find_best_nn(config, train_loader):
+def find_best_nn(config, train_loader, the_label):
 
     '''
     This function goes through all different combinations of hyperparameters 
@@ -231,14 +322,26 @@ def find_best_nn(config, train_loader):
         hyperparameters['hidden_layer_width'] = item[1]
         hyperparameters['model_depth'] = item[2]
         hyperparameters['learning_rate'] = item[3]
-        model = Neural_Network(item[1], item[2])
-        rmse_loss, r2_score, inference_latency = train(model, train_loader, 
+<        model = Neural_Network(item[1], item[2])
+        if (the_label == "Category"):
+            cross_entropy_value, accuracy, f1_score, precision, recall = train_classifier(model, train_loader, epochs, item[0], item[3])
+        else:
+            rmse_loss, r2_score, inference_latency = train(model, train_loader, 
                                                        epochs, item[0], 
                                                        item[3])
+        # end if
         datetime_end = datetime.now()
         training_duration = (datetime_end - datetime_beg).total_seconds()
-        metrics = {'RMSE_loss': float(rmse_loss)}
-        metrics['R^2 score'] = float(r2_score)
+        if (the_label == "Category"):
+            metrics = {'Accuracy': float(accuracy)}
+            metrics['F1 score'] = float(f1_score)
+            metrics['Precision'] = float(precision)
+            metrics['Recall'] = float(recall)
+            metrics['Cross entropy'] = float(cross_entropy_value)
+        else:
+            metrics = {'RMSE_loss': float(rmse_loss)}
+            metrics['R^2 score'] = float(r2_score)
+        # end if
         metrics['training_duration'] = float(training_duration)
         metrics['inference_latency'] = float(inference_latency)
         folder = 'neural_networks'
@@ -246,7 +349,11 @@ def find_best_nn(config, train_loader):
         if (rmse_loss < loss_function_baseline):
             folder = 'best_nn'
             save_model(folder, model, hyperparameters, metrics)
-            loss_function_baseline = rmse_loss
+            if (the_label == "Category"):
+                loss_function_baseline = cross_entropy_value
+            else:
+                loss_function_baseline = rmse_loss
+            # end if
         # end if
 
         # Add one second delay, just in case someone runs this with 
@@ -285,9 +392,14 @@ Parameters:
 '''
 
 # Load the input data.
-features_labels_tuple = load_airbnb("Price_Night")
-#features_labels_tuple = load_airbnb("Category")
-#features_labels_tuple = load_airbnb("bedrooms")
+the_label = "Category"  #  "Price_Night" or "bedrooms" or "Category"
+if (the_label == "Price_Night"):
+    features_labels_tuple = load_airbnb("Price_Night")
+elif (the_label == "Category"):
+    features_labels_tuple = load_airbnb("Category")
+elif (the_label == "bedrooms"):
+    features_labels_tuple = load_airbnb("bedrooms")
+# end if
 # Using sklearn to train a linear regression model to predict a feature from 
 # the tabular data. Keep one of the lines above uncommented and comment the 
 # other two. 
@@ -334,7 +446,7 @@ if __name__ == '__main__':
     elif (run_type == "neural_network"):
         # Instantiate neural network model and train it for a training set with 
         # a selection of hyperparameters. Save results of each run.
-        find_best_nn(config, train_loader)
+        find_best_nn(config, train_loader, the_label)
     # end if
 # end if
 
